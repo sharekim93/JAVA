4bcbef67c0f77263728b0aed25b902995987e553
나눔 <sksyag@naver.com>
2020-10-18 22:14:41 +0900

    update

---
+++ 200922_first_meet_java.java
@@ -0,0 +1,8 @@
+package day002;
+public class ECLIPSE_TEST2 {
+	public static void main(String[] args) {
+		System.out.println("이름 : 캡틴아메리카");
+		System.out.println("성별 : 남");
+		System.out.println("나이 : 150");
+	} // END CLASS
+} // END MAIN
