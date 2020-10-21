21b934c319035f528b82e5653b80c749ecd8653a
Administrator <Administrator@523-15>
2020-10-21 11:57:06 +0900

    JAVA-SPRING CLASS

--- Day019/src/Method023.java
+++ Day019/src/Method023.java
@@ -6,8 +6,8 @@ public class Method023 {
 		System.out.println();
 		System.out.println("[BASIC002] 배열값이 전달되는 경우");
 		int[] su= {1,3,5};
-		print (su[0],su[1]); //1,3 call by value
-		print (su); // 주소 - call by reference
+		print (su[0],su[1]); //1,3 call by value :  파라미터로 받은 값을 메서드 안에서 처리
+		print (su); // 주소 - call by reference : 주소를 따라가서 원본을 수정함.
 
 	}
 	public static void print(int i, int j) {
