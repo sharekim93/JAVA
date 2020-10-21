eb6ab2e4e7f6bc7371afdad6de882a98675acf6e
Administrator <Administrator@523-15>
2020-10-21 11:42:47 +0900

    JAVA-SPRING CLASS

--- Day019/src/Method024.java
+++ Day019/src/Method024.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 
 public class Method024 {
 
@@ -7,9 +8,16 @@ public class Method024 {
 		System.out.println("2. 배열에 들어가 있는 값 확인 : "+ abc[0]);
 		System.out.println("3. abc의 주소 : "+ System.identityHashCode(abc));
 		re_abc(abc);
-		System.out.println("4. re_abc의 주소 : "+ System.identityHashCode(abc));
+		System.out.println("5. "+Arrays.toString(abc));
+		
 	}
 	public static void re_abc(char a[]) {
-		
+		System.out.println("4. re_abc의 주소 : "+ System.identityHashCode(a));
+		System.out.println("5. 주소를 줄께 따라가서 처리해");
+		for (int i=0;i<a.length;i++) {
+			a[i]=(char)(a[i]-32);
+		}
+		//return : void
+		//parameter : char a[]
 	}
 }
