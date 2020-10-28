36f066c26af7773c51556deac3bbf6947500cef9
나눔 <sksyag@naver.com>
2020-10-28 22:34:04 +0900

    organize code using class

--- Mini_Project/src/BANK8.java
+++ Mini_Project/src/BANK8.java
@@ -1,16 +1,13 @@
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Arrays;
+
 import java.util.Scanner;
 
 class Bank_v1{
 	String id;
 	String pass;
 	double balance;
-	static String Company = "(주) 본인이름회사"; 
+	static String Company = "(주)면나눔"; 
 	
 	int menu() {
-		int num=0;
 		Scanner scanner = new Scanner(System.in);
 		
 		// 메뉴 출력
@@ -20,51 +17,114 @@ class Bank_v1{
 				+ "3.입금\r"
 				+ "4.출금\r"
 				+ "5.삭제\r"
-				+ "9.종료\r"
-				+ "입력>>> ");
-		System.out.println();
+				+ "9.종료\r");
 		
-		// 배열 출력
+		System.out.println(Company +" 계정정보");
 		System.out.println("ID : "+id+"/ PASS : "+pass+"/ BAL : "+balance);
 
-		num=scanner.nextInt();
-		return num;
+		System.out.println("\n입력>>> ");
+		return scanner.nextInt();
+
 	}
 	
 	void input() {
 		//변수
-		String temp_id=null,temp_pass=null,temp_bal=null;
+		String temp_id=null,temp_pass=null;
+		double temp_bal=0;
 		Scanner scanner = new Scanner(System.in);
 		
-		if (index!=-1) {
 		System.out.print("*ID :");
 		temp_id=scanner.nextLine();
-		
-		if(validate_id(temp_id)){
-			for(int i=0;i<users.length;i++) {
-				if(temp_id.equals(users[i][0])) {System.out.println("이미 존재하는 아이디입니다.");
-				notOverLapped=false;
-				break;}
-			}// id 유효성 체크(공백포함여부, @와 . 포함여부)
-		}
-		else return ;
-		if(!notOverLapped) {return;} // id 중복체크
-				
+		if(!validate_id(temp_id)) {return;}
 		System.out.print("*PASS :");
 		temp_pass = scanner.nextLine();
-		if(!validate_pass(temp_pass)) {return;} // password 유효성 체크(공백포함여부,비밀번호 확인)
+		if(!validate_pass(temp_pass)) {return;}
 		
 		System.out.print("*BALANCE :");
-		temp_bal = scanner.next();
-		if(!validate_bal(temp_bal)) {return;} // balance 입력값 체크 (0원 초과 입력 여부)
+		temp_bal = scanner.nextDouble();
+		if(!validate_bal(temp_bal)) {return;}
 		
-			users[index][0] = temp_id;
-			users[index][1] = temp_pass;
-			users[index][2] = temp_bal;
-			// 저장 시간 입력
-			users[index][3] = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+		this.id = temp_id;
+		this.pass = temp_pass;
+		this.balance = temp_bal;
+
 		}
-		else System.out.println("더 이상 신규가입이 불가합니다.");return;
+	
+	void show() {
+		System.out.println("======조회결과======\n\n아이디 : "
+				+this.id+"\n암호 : "
+				+this.pass+"\n잔액 : "
+				+this.balance
+				+"\n조회가 성공했습니다.\n");
+	}
+	
+	void deposit () {
+		int money=0;
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("입금할 금액을 입력해주세요 > ");
+		money = scanner.nextInt();
+		
+		if (money>=0) {this.balance =this.balance+money;}
+		else System.out.println("잘못 입력하셨습니다.");
+	}
+	
+	void withdraw() {
+		int money=0;
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("출금액 :");
+		money = scanner.nextInt();
+		if (this.balance-money>=0) {this.balance=this.balance-money;}
+		else if (this.balance-money<0) {System.out.println("금액이 모자랍니다.");}
+		else if (money<0) {System.out.println("잘못 입력하셨습니다.");}
+	}
+	
+	void delete() {
+		Scanner scanner = new Scanner(System.in);
+		String delete = null;
+			System.out.print("삭제하시겠습니까? (Y/N)");
+			delete = scanner.next();
+			if(delete.equalsIgnoreCase("Y"))
+			{this.id=null;this.pass=null;this.balance=0;}
+		}
+		
+	boolean validate_id(String id) {
+		boolean space=false;
+		boolean dot =false;
+		boolean at=false;
+		
+		for (int j=0; j<id.length();j++) {
+			if(id.charAt(j)=='\s') {space=true;}
+			if(id.charAt(j)=='@') {at=true;}
+			if(id.charAt(j)=='.') {dot=true;} 
+		}
+		if (space) {System.out.println("ID에 빈칸이 입력 되었습니다.");}
+		if (!dot) {System.out.println("ID에 .를 입력해주세요");}
+		if (!at) {System.out.println("ID에 @를 입력해주세요.");}
+		
+		if(space||!dot||!at) return false;
+		else return true;
+	}
+	
+	boolean validate_pass(String pass) {
+		boolean space=false;
+		String temp_pass =null;
+		Scanner scanner = new Scanner(System.in);
+		
+		for (int j=0; j<pass.length();j++) {
+			if(pass.charAt(j)=='\s') {space=true;}
+		}
+		if(space) {System.out.println("비밀번호에 빈칸이 입력되었습니다.");return false;}
+		System.out.println("비밀번호를 한 번 더 입력하세요 > ");
+		temp_pass = scanner.nextLine();
+		if(pass.equals(temp_pass)) return true;
+		else {System.out.println("비밀번호가 일치하지 않습니다.");return false;}
+	}
+	
+	public static boolean validate_bal(double bal) {
+		if(bal>0) {return true;}
+		else {System.out.println("입력 금액을 확인해주세요");return false;}
 	}
 	
 }
@@ -81,7 +141,7 @@ public class BANK8 {
 			case 4: user1.withdraw(); break;
 			case 5: user1.delete(); break;
 		       }
-		} while (num != 9);
+		} while (user1.menu() != 9);
 	}
 
 }
