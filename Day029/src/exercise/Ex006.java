f33b9443517686eee6d5e0f81500b94f37edd9c0
나눔 <sksyag@naver.com>
2020-11-04 20:16:24 +0900

    JAVA_SPRING CLASS

--- Day029/src/exercise/Ex006.java
+++ Day029/src/exercise/Ex006.java
@@ -1,9 +1,9 @@
-package exercise;
-
+package Day029.src.exercise;
 class Prices{
 	public final static int PRICE_AUDIO=50;
 	public final static int PRICE_TV=100;
 	public final static int PRICE_COMPUTER=200;
+	public final static int INITIAL_MONEY=1000;
 }
 
 class Product{
@@ -25,21 +25,39 @@ class Audio extends Product{
 	@Override public String toString() {return "Audio";}
 }
 class Buyer {
-	int balance;
+	int balance,spend;
 	Product[] cart;
-	
-	public Buyer() {balance=1000;cart = new Product[10];}
+	public Buyer() {balance=1000;cart = new Product[7];}
+	static int cnt=0;
 	
 	public void buy(Product p) {
-				
+		if(this.balance>p.price) {this.balance=this.balance-p.price;}
+		else return;
+		spend+=p.price;
+		cart[cnt++]=p;
+		
 	}
 	public void summary() {
-
+		int num=1;
+		String notbuy="";
+		boolean first=true;
+		
+		if(balance-Prices.PRICE_COMPUTER<0) {notbuy+="Computer, ";}
+		if(balance-Prices.PRICE_TV<0) {notbuy+="Tv, ";}
+		if(balance-Prices.PRICE_AUDIO<0) {notbuy+="Audio ";}
+		
+		System.out.println(num+".잔액이 부족하여 "+notbuy+"를 살 수 없습니다.");num++;
+		System.out.print(num+".구입한 물건 : ");
+		for(int i=0;i<cart.length;i++) {
+			if(first) {first=false;}
+			else System.out.print(",");
+			System.out.print(cart[i]);}num++;
+		System.out.println();
+		System.out.println(num+".사용한 금액 : "+spend);num++;
+		System.out.println(num+".남은 금액 : "+(Prices.INITIAL_MONEY-spend));
 		}
 		
 	}
-	
-}
 
 public class Ex006 {
 	public static void main(String[] args) {
