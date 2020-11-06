413a7afc64753dece82aa52914635ae8d5f9d6d3
Administrator <Administrator@523-15>
2020-11-06 18:02:42 +0900

    JAVA_SPRING CLASS

--- Day030/src/Exception004.java
+++ Day030/src/Exception004.java
@@ -8,8 +8,8 @@ public class Exception004 {
 	
 	public static void main(String[] args) {
 		try {
-		test(1);
-		test(0);
+		test(1); //test(1) 이상 없이 실행. 10 처리완료!
+		test(0); //바로 Exception으로 던져져서 catch문으로 넘어감
 		}
 		catch(Exception e) {System.out.println("exception 발생!");}
 
