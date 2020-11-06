fceb5ad8161730ca0ddd5241267d7d83736b13dd
Administrator <Administrator@523-15>
2020-11-06 17:54:11 +0900

    JAVA_SPRING CLASS

--- Day030/src/Exception003.java
+++ Day030/src/Exception003.java
@@ -12,7 +12,7 @@ public class Exception003 {
 			}
 			catch(Exception e){
 				System.out.println("숫자만 입력 가능합니다.");
-				scanner = new Scanner(System.in);
+				scanner.next(); // 공백처리를 해주기 위함
 			}
 			if(one==1) {break;}
 			
