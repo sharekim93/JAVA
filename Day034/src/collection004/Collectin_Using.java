c878f862eec9bb6b53d144cd461956ed87714e4a
나눔 <sksyag@naver.com>
2020-11-11 20:44:41 +0900

    JAVA_SPRING CLASS

--- Day034/src/collection004/Collectin_Using.java
+++ Day034/src/collection004/Collectin_Using.java
@@ -30,7 +30,7 @@ class Collectin_Using implements ActionListener{
 	void add() {
 		String text="";
 		text = gui.area.getText();
-		if(text.contains(" ")||text.isBlank()) {JOptionPane.showMessageDialog(null, "빈칸이 입력 되었습니다.\n확인해주세요");}
+		if(text.contains(" ")||text.isEmpty()) {JOptionPane.showMessageDialog(null, "빈칸이 입력 되었습니다.\n확인해주세요");}
 		else {JOptionPane.showMessageDialog(null,text.trim()+"을 추가했습니다.");vec.add(text.trim());}
 		gui.area.setText(null);
 	}
@@ -39,14 +39,14 @@ class Collectin_Using implements ActionListener{
 		for(int i=0;i<vec.size();i++) {
 			text+="["+i+"]"+vec.get(i)+"\n";
 		}
-		if(text==""||text.isBlank()) {JOptionPane.showMessageDialog(null, "입력된 값이 없습니다" );}
+		if(text==""||text.isEmpty()) {JOptionPane.showMessageDialog(null, "입력된 값이 없습니다" );}
 		else{JOptionPane.showMessageDialog(null, text );}
 	}
 	void get() throws Exception {
 		String text="";
 		int i=0;
 		text = gui.area.getText();
-		if(text.contains(" ")||text.isBlank()) {JOptionPane.showMessageDialog(null, "빈칸을 입력하셨습니다");}
+		if(text.contains(" ")||text.isEmpty()) {JOptionPane.showMessageDialog(null, "빈칸을 입력하셨습니다");}
 		else if (Integer.decode(text)<vec.size()){i=Integer.decode(text);JOptionPane.showMessageDialog(null, vec.get(i));}
 		else {JOptionPane.showMessageDialog(null, "잘못입력하셨습니다.");}
 		gui.area.setText(null);
@@ -55,7 +55,7 @@ class Collectin_Using implements ActionListener{
 		String text="";
 		int i=0;
 		text = gui.area.getText();
-		if(text.contains(" ")||text.isBlank()) {JOptionPane.showMessageDialog(null, "빈칸을 입력하셨습니다");}
+		if(text.contains(" ")||text.isEmpty()) {JOptionPane.showMessageDialog(null, "빈칸을 입력하셨습니다");}
 		else if (Integer.decode(text)<vec.size()){i=Integer.decode(text);JOptionPane.showMessageDialog(null, vec.remove(i)+"가 삭제 되었습니다.");}
 		else {JOptionPane.showMessageDialog(null, "잘못입력하셨습니다.");}
 		gui.area.setText(null);
--- Day034/src/collection004/Collection006_gui.java
+++ Day034/src/collection004/Collection006_gui.java
@@ -4,7 +4,7 @@ public class Collection006_gui {
 
 	public static void main(String[] args) {
 		System.out.println("[ver-1] GUI확인");
-		new CollectionGui().create();
+		//new CollectionGui().create();
 		System.out.println("\n[ver-2] MODEL 확인");
 		Collectin_Using modelTest = new Collectin_Using();
 		}
